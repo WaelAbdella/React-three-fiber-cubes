1ae6c59830c25ce1a3bd78f69b4ab4dc692124e5
Wael Ahmed <80431088+WaelAbdella@users.noreply.github.com>
2021-05-12 00:05:24 +0400

    adding a forth cube to test netlify build with github

--- src/App.js
+++ src/App.js
@@ -80,6 +80,7 @@ function App() {
           />
           <SpinningMesh position={[-2, 1, -5]} color='pink' speed={6} />
           <SpinningMesh position={[5, 1, -2]} color='pink' speed={6} />
+          <SpinningMesh position={[6, 1, -3]} color='pink' speed={6} />
         </group>
         <OrbitControls />
       </Canvas>
